ea09a7fd948da069e62f29c30df91823d294e45f
Dilan <nigrenalid@hotmail.com>
2017-12-04 17:13:40 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# BasitKisiselBlog-MVCproject
---
+++ dilan/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace dilan.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult Hakkimda()
+        {
+            return View();
+        }
+
+        public ActionResult Projeler()
+        {
+            return View();
+        }
+
+        public ActionResult İletisim()
+        {
+            return View();
+        }
+
+
+    }
+}
